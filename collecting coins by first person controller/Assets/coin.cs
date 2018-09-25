f52cb500f8826713ad8fc9c5d1b48fca45ada792
satyamws <satyam.ws@gmail.com>
2018-09-25 18:28:56 +0530

    collecting coins
    Collecting coins by first person controller in a snowy place

---
+++ collecting coins by first person controller/Assets/coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.name=="Player")
+		{
+			other.GetComponent<PlayerScript>().points++;
+			Destroy(gameObject);
+		}
+	}
+}
