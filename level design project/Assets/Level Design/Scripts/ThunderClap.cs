ef977807b23acb8f7e7aaf94adfb367ea732de94
satyamws <satyam.ws@gmail.com>
2018-09-26 08:28:47 +0530

    level design
    Creation of a basic level design

---
+++ level design project/Assets/Level Design/Scripts/ThunderClap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThunderClap : MonoBehaviour
+{
+    bool canFlicker = true;
+    public AudioClip clip;
+
+    void Update()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+
+    {
+        if (canFlicker)
+        {
+            canFlicker = false;
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+            GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 5));
+            canFlicker = true;
+        }
+    }
+
+}
